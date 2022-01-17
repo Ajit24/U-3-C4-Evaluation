f262bd163272c6b4969f17a08360a87cb341c162
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-01-17 17:04:57 +0530

    first commit

---
+++ components/sidebar.js
@@ -0,0 +1,18 @@
+function sidebar() {
+    return `<div id="sidebar">
+    
+              <h1>TechCrunch</h1>
+          </div>
+          <div id="login">
+            
+      <h2>Startups</h2>
+          <h2>Videos</h2>
+          <h2>News Letter</h2>
+          <h2>Audio</h2>
+          </div>
+      </div>
+      <div id="newsbar">
+          <div id="append"></div>
+      </div>`;
+  }
+  export default sidebar;
